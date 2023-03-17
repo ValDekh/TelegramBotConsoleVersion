52b43c5d7682e1aa3b65b3fbfcd785ba1760eceb
Valerii Dekhtiuk <Dekhtiuk.v.s@gmail.com>
2023-03-17 02:22:39 +0200

    Finale

--- TelegramBotConsoleVersion/Program.cs
+++ TelegramBotConsoleVersion/Program.cs
@@ -3,14 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TelegramBotConsoleVersion.Services;
 
-//using IHost host = Host.CreateDefaultBuilder(args)
-//    .ConfigureAppConfiguration(config =>
-//    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true))
-//    .ConfigureServices(services =>
-//    services.AddSingleton<ICosmosDBSetter, CosmosDBSetter>()
-//            .AddSingleton<IMyCustomTelegramBot, MyCustomTelegramBot>()
-//).Build();
-// await host.RunAsync();
 
 
 using IHost host = CreateHostBuilder(args).Build();
